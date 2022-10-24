90efc7b3b985f7f548e2f6c01acf9ea9c6f356a4
devWonil <102580743+devWonil@users.noreply.github.com>
2022-10-24 09:07:17 +0900

    .

--- src/main/java/com/stussy/stussyclone20220929wonil/controller/admin/api/ProductApi.java
+++ src/main/java/com/stussy/stussyclone20220929wonil/controller/admin/api/ProductApi.java
@@ -8,11 +8,10 @@ import com.stussy.stussyclone20220929wonil.dto.validation.ValidationSequence;
 import com.stussy.stussyclone20220929wonil.service.admin.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/admin")
 @RestController
@@ -25,8 +24,27 @@ public class ProductApi {
     @LogAspect
     @PostMapping("/product")
     public ResponseEntity<?> addProduct(@Validated(ValidationSequence.class) ProductAdditionReqDto productAdditionReqDto, BindingResult bindingResult) throws Exception {
+
+//        String productName = productAdditionReqDto.getName();
+//
+//        for(int i = 0; i < 200; i++){
+//          if(i % 4 == 0){
+//              productAdditionReqDto.setName(productName + "-" + (i + 1));
+//          }
+//          productService.addProduct(productAdditionReqDto);
+//        }
+
+
         return ResponseEntity
                 .created(null)
                 .body(new CMRespDto<>(1, "Successfully", productService.addProduct(productAdditionReqDto)));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<?> getProductList(@RequestParam int pageNumber,
+                                            @RequestParam @Nullable String category,
+                                            @RequestParam @Nullable String searchText) {
+
+        return ResponseEntity.ok(new CMRespDto<>(1, "Successfully", null));
     }
 }
